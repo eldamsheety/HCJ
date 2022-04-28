78871543039d6d49a7c40ab9e04ed77686c1751d
Omar Eldamsheety <omar198@gmail.com>
2022-04-28 13:44:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// index.js file 
+// index.js file
+
+document.getElementById("btochbgcolor").onclick = function changbackgroundcolor(){
+    document.getElementById('body').style.backgroundColor = document.getElementById("changebgcolor").value;
+}
